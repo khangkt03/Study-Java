aed1c1ea61c69ce4bc969a3c33636b339416938f
khangkt03 <duykhangkt03@gmail.com>
2025-10-21 09:37:45 +0700

    bai1

---
+++ src/Bai1/HelloWorld.java
@@ -0,0 +1,8 @@
+package Bai1;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World with Java ");
+		System.out.println("Hello World with Java ");
+	}
+}
